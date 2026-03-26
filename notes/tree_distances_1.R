d7ee82756b4a3f7756fcdcf29e8345e3ba5eb113
Nicholas J. Matzke <matzke@berkeley.edu>
2026-03-26 13:40:19 +1300

    starter

---
+++ notes/tree_distances_1.R
@@ -0,0 +1,9 @@
+phangorn::treedist
+symmetric.difference
+branch.score.difference
+path.difference
+quadratic.path.difference
+phangorn::RF.dist
+phangorn::wRF.dist
+phangorn::sprdist
+TreeDist::MatchingSplitInfoDistance(tree1=iqtree_cutdown, tree2=master_tr_cutdown, normalize=TRUE)
